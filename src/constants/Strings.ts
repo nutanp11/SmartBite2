f04d398bb7990a0b6e6a2aa729b48a6d98e01a53
nutanp11 <nutan.pachangane@capgemini.com>
2025-03-27 15:23:35 +0530

    first commit

---
+++ src/constants/Strings.ts
@@ -0,0 +1,11 @@
+export const SEARCH_TEXT = 'What do you looking for?';
+export const WELCOME_TEXT = 'Welcome Back, ';
+export const ENTER_INFO = 'Please type your information';
+export const ENTER_EMAIL = 'Email address';
+export const PASSWORD = 'Password';
+export const NEW_USER = 'New user? ';
+export const CREATE_ACCOUNT = 'Create an account';
+export const ALREADY_ACCOUNT = 'Already have an account?';
+export const LOG_IN = 'Login';
+export const FULL_NAME = "Full name";
+export const CREATE_ACC_BTN = 'Create Account'
